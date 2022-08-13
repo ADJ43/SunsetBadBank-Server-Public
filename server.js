5c8f50331b482ef4f92694ebf72e811f6ca85353
ADJ43 <adjse43@gmail.com>
2022-08-13 16:12:03 -0400

    First Commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record.js"));
+// get driver connection
+const dbo = require("./db/conn.js");
+const { application } = require("express");
+
+app.get("/", (req, res) => {
+  res.send("Hello World from Sunset Bad Bank Server");
+});
+ 
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
+});
